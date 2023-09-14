2ba36c4123b1473ed160d1f893b412b2d6a0e9c1
nnawar706 <nnawar706@gmail.com>
2023-09-15 01:21:54 +0600

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+
 import {
     logo,
     profile,
@@ -9,21 +12,19 @@ import {
     students,
 } from "../images";
 
-const Sidebar = () => (
-    <section className="sidebar">
+const Sidebar = () => {
+    const location = useLocation();
+
+    return (
+    <div className="sidebar">
         <div className="logoContainer">
             <img src={logo} alt="logo" className="logo" />
             <h3 className="title">
                 <u>QUIZBOT.</u>
             </h3>
         </div>
-        <div className="burgerContainer">
-            <div className="burgerTrigger">
-                <div className="burgerMenu"></div>
-            </div>
-        </div>
         <div className="profileContainer">
-            <img src={profile} alt="profile" className="profile" />
+            {/* <img src={profile} alt="profile" className="profile" /> */}
             <div className="profileContents">
                 <p className="name">Hello, John</p>
                 <p>john@hello.com</p>
@@ -31,33 +32,29 @@ const Sidebar = () => (
         </div>
         <div className="contentsContainer">
             <ul>
-                <li className="active">
+                <li className={location.pathname == '/exam-room/:id' ? "active" : ""}>
                     <img src={dashboard} alt="dashboard"/>
-                    <a href="/">Dashboard</a>
+                    <a href="/exam-room/:id">Room</a>
                 </li>
-                <li className="active">
+                <li className={location.pathname == '/exam-room/:id/setting' ? "active" : ""}>
                     <img src={settings} alt="exam-rooms"/>
-                    <a href="/">Settings</a>
+                    <a href="/exam-room/:id/setting">Settings</a>
                 </li>
-                <li>
+                <li className={location.pathname == '/exam-room/:id/students' ? "active" : ""}>
                     <img src={students} alt="exam-rooms"/>
-                    <a href="/">Students</a>
+                    <a href="/exam-room/:id/students">Students</a>
                 </li>
-                <li>
+                <li className={location.pathname == '/exam-room/:id/quizzes' ? "active" : ""}>
                     <img src={quizzes} alt="exam-rooms"/>
-                    <a href="/">Quizzes</a>
+                    <a href="/exam-room/:id/quizzes">Quizzes</a>
                 </li>
-                <li>
+                <li className={location.pathname == '/exam-room/:id/results' ? "active" : ""}>
                     <img src={performance} alt="exam-rooms"/>
-                    <a href="/">Result</a>
+                    <a href="/exam-room/:id/results">Result</a>
                 </li>
-                {/* <li className="active">
-                    <img src={performance} alt="exam-rooms"/>
-                    <a href="/">Performance</a>
-                </li> */}
             </ul>
         </div>
-    </section>
-);
+    </div>
+)};
 
 export default Sidebar;

327c6fd3404c0d1ac2a0157aeaff9a51598f2027
nnawar706 <nnawar706@gmail.com>
2023-09-16 00:06:22 +0600

    login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
+import { Provider } from "react-redux";
 
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
@@ -8,17 +9,20 @@ import Dashboard from "./containers/Dashboard";
 import Login from "./containers/Login";
 import Register from "./containers/Register";
 import "./styles/main.scss";
+import { store } from "./store";
 
 const App = () => {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
 
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import authReducer from "./features/auth";
+
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+    },
+    devTools: process.env.NODE_ENV !== 'production',
+});

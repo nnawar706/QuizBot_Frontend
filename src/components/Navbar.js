387bb6edf943640b992241c1f8f51e17d9e3b675
nnawar706 <nnawar706@gmail.com>
2023-09-19 00:52:36 +0600

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,19 @@
+import { BsPlusSquareFill } from "react-icons/bs";
+
 const Navbar = () => (
     <>
-        <h2>Navbar here</h2>
+        <section className="sticky top-0 z-30 w-full h-20 px-2 py-4 bg-white sm:px-4">
+            <div className="flex items-center justify-end mx-auto max-w-7xl">
+                <div className="flex items-center space-x-1">
+                    <ul className="hidden space-x-2 md:inline-flex">
+                        <li>
+                            <BsPlusSquareFill className="h-7 w-7 text-dark-green mt-2"/>
+                            <BsPlusSquareFill className="h-7 w-7 text-dark-green mt-2"/>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     </>
 );
 
--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,13 +1,15 @@
 import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
 const Dashboard = () => (
     <Layout title="QuizBot | Dashboard" content="Admin Dashboard">
-        <section className="flex">
+        <section className="flex bg-very-light-green">
             <Sidebar/>
-            <section className="p-7">
+            <Navbar/>
+            {/* <section className="p-7 ">
                 <h2 className="text-2xl font-semibold">Dashboard Here</h2>
-            </section>
+            </section> */}
         </section>
     </Layout>
 );

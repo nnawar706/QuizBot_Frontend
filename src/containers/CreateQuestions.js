0468540fd0262b6f12221369f7591aa336926b0a
nnawar706 <nnawar706@gmail.com>
2023-10-25 01:21:25 +0600

    questions

--- src/containers/CreateQuestions.js
+++ src/containers/CreateQuestions.js
@@ -1,3 +1,24 @@
-const Questions = () => {}
+import Layout from "../components/Layout"
+import Sidebar from "../components/Sidebar"
+import Navbar from "../components/Navbar"
+
+const Questions = () => {
+    return (
+        <Layout title="QuizBot | Quiz" content="Quiz">
+            <section className="flex bg-very-light-green">
+                <Sidebar />
+                <div className="w-full">
+                    <Navbar />
+
+                    <div className="p-6">
+                        <div className="bg-white rounded-md p-3 min-h-[700px]">
+                            <h1 className="text-dark-green font-bold text-[22px] mb-8">Quiz Question</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </Layout>
+    )
+}
 
 export default Questions
